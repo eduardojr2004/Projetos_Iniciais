bbb756a65ee0fc96c9ddabb4e21525982982595d
eduardojr2004 <97413115+eduardojr2004@users.noreply.github.com>
2026-09-01 18:53:15 -0300

    Codigo boletim

---
+++ boletim.py
@@ -0,0 +1,8 @@
+disciplinas = ("matemática", "português", "filosofia", "história",
+               "física", "geografia","química","biologia")
+notas = [7.8, 8.2, 9.5, 5.7, 9.8, 10, 6.4, 7.0]
+
+for i in range(len(disciplinas)):
+    print(f"A nota de {disciplinas[i]} foi {notas[i]}")
+
+print("Final das notas")
