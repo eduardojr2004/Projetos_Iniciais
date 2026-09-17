69b98091d35eb6e935927ad8685a7a4e621cc3e9
eduardojr2004 <97413115+eduardojr2004@users.noreply.github.com>
2026-09-16 21:59:01 -0300

    ajustes

--- enumerate.py
+++ enumerate.py
@@ -4,5 +4,6 @@ for i in range(3):
     nome = input(f"Informe o nome do {i + 1}º produto: ")
     lista.append(nome)
 
+#O i RECEBE A POSICAO E NOME RECEBE O NOME DA LISTA
 for i, nome in enumerate(lista):
     print(i, nome)
